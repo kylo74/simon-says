5b65567340691a7fd37406e211ff876e8432cff8
kylo74 <kyloc74@gmail.com>
2022-03-30 09:36:04 +0000

    Add ids to choices array

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -16,4 +16,16 @@ describe("gmae object contains correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
     });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 });
